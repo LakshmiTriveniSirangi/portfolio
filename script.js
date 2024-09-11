a91c32f08751678dc5518c2a4ebf53a1557932ad
Lakshmi Triveni Sirangi <141419184+LakshmiTriveniSirangi@users.noreply.github.com>
2024-09-11 23:55:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var typed=new Typed(".text",{
+    strings:["Frontend Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    })
+// Select the menu icon and navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let closeBtn = document.querySelector('#close-btn');
+
+// Add click event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+// Add click event listener to the close button
+closeBtn.addEventListener('click', () => {
+    navbar.classList.remove('active');
+});
+
+// Handle scroll events
+window.addEventListener('scroll', () => {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Close the navbar on scroll if it's open
+    if (navbar.classList.contains('active')) {
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    }
+});
